19d70b262534430ff122088e23b56df6a8a4324d
D. LoBraico <dj@lobraico.com>
2012-05-18 20:21:32 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linus::Application.config.secret_token = 'e3e96a05ec278052834dee0dbb3c8a994a6188a047d36a06e417cce942391050f969444bdd5b0e772f373124f32e264f8bae23aa74e9cb967e928748a4ff9dfb'
