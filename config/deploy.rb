5c951ad99dfdfefdde1b4c78af80dd96c05b236d
D. LoBraico <dj@lobraico.com>
2012-05-21 03:50:44 -0500

    deploy.rb should now symlink db.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,6 +27,14 @@ default_run_options[:pty]         = true
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
+after 'deploy:update_code', 'deploy:symlink_db'
+
+namespace :deploy do
+  desc "Symlinks the database.yml"
+  task :symlink_db, :roles => :app do
+    run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
+  end
+end
 
 namespace :deploy do
   task :start do ; end
