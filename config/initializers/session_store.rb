4782c32092b0dfa6542dd45a0499cf472d4ed59d
D. LoBraico <dj@lobraico.com>
2012-05-20 21:10:50 -0500

    moves session store to db

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Linus::Application.config.session_store :cookie_store, key: '_linus_session'
+#Linus::Application.config.session_store :cookie_store, key: '_linus_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Linus::Application.config.session_store :active_record_store
+Linus::Application.config.session_store :active_record_store
