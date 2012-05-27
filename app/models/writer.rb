71dad0f9ad77a17009700a43079ec559c96a4a65
D. LoBraico <dj@lobraico.com>
2012-05-26 20:42:35 -0500

    better styling of flash notices

--- app/models/writer.rb
+++ app/models/writer.rb
@@ -13,4 +13,6 @@ class Writer < ActiveRecord::Base
   has_many :assignments 
   has_many :ideas, :through => :assignments, :dependent => :nullify
   attr_accessible :name, :email
+
+  validates_uniqueness_of :email
 end
