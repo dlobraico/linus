5126159a598d6ae6ce81980071ab59e4f8c0d316
D. LoBraico <dj@lobraico.com>
2012-08-15 00:38:50 -0400

    sanitize input for submissions

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -13,4 +13,18 @@
 class Submission < ActiveRecord::Base
   attr_accessible :body, :byline, :headline
   belongs_to :issue
+
+  before_validation :sanitize_body, :on => :create
+  before_validation :sanitize_body, :on => :update
+
+  private
+  def sanitize_body
+    self.body = sanitize self.body
+  end
+
+  def sanitize field
+    ActionController::Base.helpers.sanitize(field,
+                                       :tags => %w(a b i strong em p h1 h2 h3 h4 h5 h6),
+                                       :attributes => %w(href name src type value width height data))
+  end
 end
