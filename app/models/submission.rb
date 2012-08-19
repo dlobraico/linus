87e850cf20ffda3420c203729a8b950f5649c27b
D. LoBraico <dj@lobraico.com>
2012-08-19 09:40:11 +0800

    adds some todos

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -25,9 +25,10 @@ class Submission < ActiveRecord::Base
   end
 
   def sanitize field
-    result = ActionController::Base.helpers.sanitize(field,
-                                       :tags => %w(a b i strong em p h1 h2 h3 h4 h5 h6),
-                                       :attributes => %w(href name src type value width height data))
+    result =
+      ActionController::Base.helpers.sanitize(field,
+        :tags => %w(a b i strong em p h1 h2 h3 h4 h5 h6),
+        :attributes => %w(href name src type value width height data))
     result.gsub(/&nbsp;/, ' ')
   end
 end
