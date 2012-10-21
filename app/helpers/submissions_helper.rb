11ee07ccabfb9aa54b064b97253c4055d5678139
D. LoBraico <dj@lobraico.com>
2012-10-21 14:30:59 -0500

    automatically fill byline field

--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -1,2 +1,9 @@
 module SubmissionsHelper
+  def byline_or_writer_field(form, submission)
+    unless @submission.writer.nil?
+      form.text_field :byline, :value => @submission.writer.name
+    else
+      form.text_field :byline, :placeholder => "Byline"
+    end
+  end
 end
