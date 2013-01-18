a6ef5844e7132cc0d10a04ca2ac251436379a6e8
D. LoBraico <dj@lobraico.com>
2013-01-17 18:21:08 -0600

    articles controller should allow grabbing all articles not just the current issue or a specified issue

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,9 +3,12 @@ class ArticlesController < ApplicationController
   # GET /articles.json
   def index
     @issue =
-      id = if params[:issue_id].nil? then Issue.last_issue else params[:issue_id] end
-      Issue.find_by_id(id)
-    @articles = Submission.where("issue_id = ? AND published = ?", @issue, true)
+      unless params[:issue_id].nil? 
+        issue = Issue.find_by_id(id)
+        @articles = Submission.where("issue_id = ? AND published = ?", issue, true)
+      else
+        @articles = Submission.where("published = ?", true)
+      end
 
     respond_to do |format|
       format.json { render json: @articles.to_json(:include => {:images => {:only => [:description, :file]}}) }
