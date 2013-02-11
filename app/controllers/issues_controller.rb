dcc75cdac0ed0da6d35fd0280cb72ebf9fef4d15
D. LoBraico <dj@lobraico.com>
2013-02-10 20:48:49 -0600

    order issues in a logical way

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -4,7 +4,7 @@ class IssuesController < ApplicationController
   # GET /issues
   # GET /issues.json
   def index
-    @issues = Issue.all
+    @issues = Issue.order("distribution DESC")
 
     respond_to do |format|
       format.html # index.html.erb
