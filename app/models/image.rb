ace105039acff2367b7a8981b598bac8835cb192
D. LoBraico <dj@lobraico.com>
2013-03-07 02:56:32 -0600

    separate writer and image creators for submissions

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,7 @@
 class Image < ActiveRecord::Base
   belongs_to :imageable, :polymorphic => true
   belongs_to :writer
-  attr_accessible :description, :file, :remove_file
+  attr_accessible :description, :file, :remove_file, :writer_id
   mount_uploader :file, ImageUploader
   
   attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
