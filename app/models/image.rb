1df5d53bbf1a9aae680b942a23655c6c1dcbd378
D. LoBraico <dj@lobraico.com>
2013-03-13 16:39:57 -0500

    adds credit field to submission form

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,8 +1,8 @@
 class Image < ActiveRecord::Base
   belongs_to :imageable, :polymorphic => true
   belongs_to :writer
-  attr_accessible :description, :file, :remove_file, :writer_id
+  attr_accessible :description, :file, :remove_file, :writer_id, :credit
   mount_uploader :file, ImageUploader
-  
+
   attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
 end
